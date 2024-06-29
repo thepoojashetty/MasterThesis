089ca961e08a4aac8a194c1f94aa23d79035ca7a
thepoojashetty <03poojashetty@gmail.com>
2024-06-29 12:03:44 +0200

    SLURM batch script for cityscapes preprocess

--- tiny_seg_cityscapes.sh
+++ tiny_seg_cityscapes.sh
@@ -6,10 +6,9 @@
 #SBATCH -e nnUNet.err
 #Timelimit format: "hours:minutes:seconds" -- max is 24h
 #SBATCH --time=24:00:00
-#SBATCH --ntasks-per-node=2
 
 module load python
 conda activate nnunet
 
-export nnUNet_raw="./nnUNet_raw"
+export nnUNet_raw="/home/hpc/iwi5/iwi5192h/MasterThesis_nnUNet/nnUNet_raw"
 srun python nnunetv2/dataset_conversion/Dataset_Cityscapes.py source="/home/woody/iwi5/iwi5192h/Datasets/Cityscapes"
